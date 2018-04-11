f9e62e00994208c1d210488350693b550140e89d
Sasanka <sasanka@calcey.com>
2018-04-11 10:25:39 +0530

    Passing json object instead of a string

--- src/main/java/WebApp/HelloWorld.java
+++ src/main/java/WebApp/HelloWorld.java
@@ -6,34 +6,42 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.google.gson.JsonObject;
+//import com.google.gson.JsonObject;
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonWriter;
 
 
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/helloworld")
 public class HelloWorld {
-    // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/plain"
-    @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-
-
-        return "Coming from restAPI";
-    }
-
-
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response getClichedMessage() {
-//        JsonObject jsonObject = new JsonObject();
-//        jsonObject.addProperty("name","Sasanka");
-//
-//        // Return some cliched textual content
-//        String text = "Finally it worked bitch";
-//
-//        return Response.status(200).entity(jsonObject.toString()).build();
+//    @Produces("text/plain")
+//    public String getClichedMessage() {
+//        return "Coming from restAPI test test test";
 //    }
+
+
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getClichedMessage() {
+        JsonObject personObject = Json.createObjectBuilder()
+                .add("title", "Donuld trump decide to launch the neuclear bomb")
+                .add("date", "10/04/2018")
+                .add("image", "https://timedotcom.files.wordpress.com/2018/03/donald-trump-trade-wars-change-mind.jpg")
+                .add("description", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.\n"
+                )
+                .add("phoneNumber",
+                        Json.createArrayBuilder().add("00-000-0000")
+                                .add("11-111-1111")
+                                .add("11-111-1112")
+                                .build()
+                )
+                .build();
+
+        return Response.status(200).entity(personObject.toString()).build();
+    }
 
 
 }
